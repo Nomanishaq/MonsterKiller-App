0aad48d51b22e7fe7b94d0fd0558f0d67f45ead7
Nomanishaq <nomanishaq251@gmail.com>
2020-02-16 19:54:57 +0500

    add strong attack handler

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,5 @@
 const attackValue = 10;
+const strongAttackHandlerValue = 17;
 const monsterAttackValue = 14;
 
 let choseMaxLife = 100;
@@ -7,21 +8,36 @@ let currentPlayerHealth = choseMaxLife;
 
 adjustHealthBars(choseMaxLife)
 
-let attackHandler = () =>{
-    const damage = dealMonsterDamage(attackValue);
-    currentMonsterHealth -= damage;
-    const  playerDamage = dealPlayerDamage(monsterAttackValue);
-    currentPlayerHealth -= playerDamage;
-    
-
-    if (currentMonsterHealth <= 0) {
-        alert('yoyo');
-    } else if (condition) {
-        
+let attackMonster = mode => {
+    let maxDamage;
+    if (mode == 'attack') {
+        maxDamage = attackValue;
+    } else if (mode == 'strongAttack') {
+        maxDamage = strongAttackHandlerValue;
     }
+
+    const damage = dealMonsterDamage(maxDamage);
+    currentMonsterHealth -= damage;
+    const playerDamage = dealPlayerDamage(monsterAttackValue);
+    currentPlayerHealth -= playerDamage;
+
+    if (currentMonsterHealth <= 0 && currentPlayerHealth > 0) {
+        alert('yoyo');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth > 0) {
+        alert('lost');
+    } else if (currentPlayerHealth <= 0 && currentMonsterHealth <= 0) {
+        alert('drow');
+    }
+
 }
 
+let attackHandler = () => {
+    attackMonster('attack')
+}
 
+let strongAttackHandler = () => {
+    attackMonster('strongAttack')
+}
 
-
-attackBtn.addEventListener('click',attackHandler);
+attackBtn.addEventListener('click', attackHandler);
+strongAttackBtn.addEventListener('click', strongAttackHandler);
